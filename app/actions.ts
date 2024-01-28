be743fe9fe22c182ce0a5b5fca50aef594d1ab7b
vipansh <vipanshthakur@gmail.com>
2024-01-28 05:52:04 +0530

    ✅feat: segmind-npm added

--- app/actions.ts
+++ app/actions.ts
@@ -1,73 +1,77 @@
 "use server";
 
 import { ApplicationError } from "@/lib/error";
-import { callSigmindAPI } from "@/lib/sigmind";
 import { supabaseClient } from "@/lib/supabase";
 import OpenAI from "openai";
 import { decode } from "base64-arraybuffer";
+import { SDXL } from "segmind-npm";
+import { randomUUID } from "crypto";
 
+const apiKey = process.env.NEXT_PUBLIC_SIGMIND_API_KEY;
 const openAiKey = process.env.OPENAI_KEY;
 const projectId = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 export async function createCompletion(prompt: string): Promise<any> {
   if (!prompt) {
-    console.log("Prompt is missing");
     throw new Error("Prompt is missing");
   }
 
   if (!openAiKey) {
-    console.log("Missing environment variable OPENAI_KEY");
     throw new ApplicationError("Missing environment variable OPENAI_KEY");
   }
   if (!projectId) {
-    console.log("Database URL is missing");
     throw new ApplicationError("Database URL is missing");
   }
+  if (!apiKey) {
+    throw new ApplicationError("Segmind api key missing");
+  }
 
-  console.log("Creating a new OpenAI instance");
   const openai = new OpenAI({ apiKey: openAiKey });
+  const sdxl = new SDXL(apiKey);
 
-  // Generate blog post using OpenAI
-  console.log("Generating blog post using OpenAI");
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: "Say this is a test" }],
     model: "gpt-3.5-turbo",
   });
 
   const blogContent = chatCompletion.choices[0].message?.content;
-  console.log("Blog post generated");
 
-  // Generate an image using Sigmind
-  console.log("Generating an image using Sigmind");
-  const sigmindResponse = await callSigmindAPI({ prompt });
+  const sigmindResponse = await sdxl.generate({
+    prompt: prompt,
+    style: "base",
+    samples: 1,
+    scheduler: "UniPC",
+    num_inference_steps: 25,
+    guidance_scale: 8,
+    strength: 0.2,
+    high_noise_fraction: 0.8,
+    seed: 468685,
+    img_width: 1024,
+    img_height: 1024,
+    refiner: true,
+    base64: false,
+  });
   const imageBuffer = sigmindResponse.data;
   if (!imageBuffer) {
-    console.log("Error generating image");
     throw new ApplicationError("Error generating image");
   }
-  console.log("Image generated");
 
-  // Upload the image to Supabase storage
-  console.log("Uploading the image to Supabase storage");
+  const filePath = randomUUID() + "-";
   const { data, error } = await supabaseClient.storage
     .from("images")
-    .upload(`blog-${Date.now()}`, decode(imageBuffer), {
+    .upload(`blog-${filePath}`, decode(imageBuffer), {
       contentType: "image/png",
     });
 
   if (error) {
-    console.log("Unable to upload image", error);
     throw new ApplicationError("Unable to upload image");
   }
-  console.log("Image uploaded");
 
   const path = data.path;
   const imageUrl = `${projectId}.supabase.co/storage/v1/object/public/images/${path}`;
 
-  // Create a new blog post in Supabase on blog table passing title, content, image_url, and userId
-  console.log("Creating a new blog post in Supabase");
   const { data: blogData, error: blogError } = await supabaseClient
-    .from("blog")
+    .from("blogs")
     .insert([
       {
         title: prompt,
@@ -79,10 +83,8 @@ export async function createCompletion(prompt: string): Promise<any> {
     .select();
 
   if (blogError) {
-    console.log("Failed to create blog post", blogError);
     throw new Error("Failed to create blog post");
   }
-  console.log("Blog post created successfully");
 
   return blogData;
 }
