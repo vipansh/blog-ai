e7634ddb9e53d082fc0383f2a2114266dedcce51
vipansh <vipanshthakur@gmail.com>
2024-01-29 17:19:23 +0530

    fix: runtime removed

--- app/actions.ts
+++ app/actions.ts
@@ -111,10 +111,6 @@ async function createBlogPostInSupabase(
   return data[0].id;
 }
 
-export const config = {
-  runtime: "edge",
-};
-
 export async function createCompletion(prompt: string): Promise<void> {
   try {
     validateInputs(prompt);
